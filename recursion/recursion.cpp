0ac232bde4480329f1422bfcf86661603ab1c7d6
Artem <artemis.zaytsev@gmail.com>
2022-10-08 01:20:56 +0300

    Adding recursion task

---
+++ recursion/recursion.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int factorial(int n) {
+	return n > 1 ? n * factorial(n - 1) : 1;
+}
+
+int fibonacci(int n) {
+	return n == 0 ? 0 : n == 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		printf("%d! = %d\n", i, factorial(i));
+	}
+	printf("\n\n");
+	for (int i = 0; i <= 10; i++) {
+		printf("Fibonacci(%d) = %d\n", i, fibonacci(i));
+	}
+}
---
+++ str_xy_len/str_xy_len.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int count(char* string, char x, char y) {
+	int length = strlen(string);
+	int flag = 0; // 0 - didn't meet, 1 - already met
+	int counter = -1;
+	int max_counter = -1;
+	char cur;
+	for (int i = 0; i < length; i++) {
+		cur = string[i];
+		if (flag == 0 && (cur == x || cur == y)) {
+			flag = 1;
+			counter = 1;
+		} else if (cur == x || cur == y) {
+					if (max_counter < counter) { max_counter = counter; }
+			} else if (cur == '0') { counter++; }
+	}
+	return max_counter;
+}
+
+int main() {
+	char string1[] = "000XY00X000";
+	printf("%d ", count(string1, 'X', 'Y'));
+	char string2[] = "00X00Y00Y00Y";
+	printf("%d", count(string2, 'X', 'Y'));
+
+}

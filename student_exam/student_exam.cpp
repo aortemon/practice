0421cb9a6e67ecc9e74e95abb7edeb5f6c8e4528
Artem <artemis.zaytsev@gmail.com>
2022-10-13 17:11:51 +0300

    Practice 13.10

---
+++ student_exam/student_exam.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int average(int* arr, int count) {
+	int sum = 0;
+	for (int i = 0; i < count; i++) sum += arr[i];
+	return sum / count;
+}
+
+void func(int* p, int count) {
+	int level = average(p, count);
+	switch (level) {
+	case 5: {printf("\nOtlichnik"); }
+	case 4: {printf("\nKhoroshist"); }
+	case 3: {printf("\nTroechnik"); }
+	case 2: {printf("\nDvoechnik"); }
+	default: { printf("An error occured");
+	}
+
+	}
+}
+
+
+int main() {
+	
+	const int count = 3;
+	int levels[count] = { 2, 3, 4 };
+
+	func(levels, 3);
+
+	system("pause");
+	return 1;
+}
---
+++ task1/task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int validate(int value) {
+	switch (value) {
+	case 15: {return 15; break; }
+	case 16: {return 16; break; }
+	case 17: {return 17; break; }
+	case 18: {return 18; break; }
+	default: return -1;
+	}
+}
+
+int main() {
+	int arr[] = { 11, 12, 13, 14, 0, 47, 15, 16, 17, 18, 19 };
+	int temp;
+	for (int i = 0; i < 11; i++) {
+		temp = validate(arr[i]);
+		if (temp != -1) printf(" %d\n", temp);
+	}
+
+	return 0;
+}

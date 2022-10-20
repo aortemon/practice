3ef0e71ff25a02d8906f27d30e8c96e0a567a422
Artem <artemis.zaytsev@gmail.com>
2022-10-20 17:18:45 +0300

    Classwork 20.10.2022

---
+++ malloc/malloc.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE 5
+
+int* create_array(int length) {
+	int* arr = (int*)malloc(length * sizeof(int));
+	printf("Input: \n");
+	for (int i = 0; i < length; i++) {
+		printf("a[%d] = ", i);
+		scanf_s("%d", &arr[i]);
+	}
+	return arr;
+}
+
+
+int main() {
+	/*int* arr = create_array(SIZE);
+	printf("\n\nOutput: \n");
+	for (int i = 0; i < SIZE; i++) printf(">>> %d\n", arr[i]);
+	free(arr);*/
+	unsigned int x = 234567891;
+	printf("%d\n", x);
+	unsigned int y = x << 8;
+	printf("%d\n", y);
+	unsigned int z = y >> 24;
+	printf("%d\n", z);
+
+
+
+}
---
+++ ФИИТ/ФИИТ.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int find_nearest(int* arr, int num, int length) {
+	int min_dif = abs(num - arr[0]), res = arr[0]; 
+	for (int i = 0; i < length; i++) {
+		if (abs(arr[i] - num) < min_dif) {
+			res = arr[i];
+			min_dif = abs(arr[i] - num);
+		}
+	}
+	return res; 
+}
+
+int main() {
+	setlocale(LC_CTYPE, "ru");
+	int arr[] = { 10, 1, 15, 3, 55, 16, 50, 49 };
+	int num = 70;
+	printf("Ближайшее значение: %d", find_nearest(arr, num, sizeof(arr) / sizeof(arr[0])));
+
+}

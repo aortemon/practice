6eb7dcc379a90456a3217d9e39dc0e1abefe3aef
Artem <artemis.zaytsev@gmail.com>
2022-11-10 17:00:48 +0300

    Successfully working with files

---
+++ files/files.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <fstream>
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <istream>
+#include <Windows.h>
+#include <locale.h>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_CTYPE, "Rus");
+	char name[250];
+	string s;
+	cout << "enter the name of the file \n";
+	cin.getline(name, 250);
+	FILE* f;
+	f = fopen(name, "r");
+	int size;
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	//fseek(f, 0, SEEK_SET);
+	char buf(size);
+
+	while (!feof(f)) {
+		fread(&buf, sizeof(buf), 1, f);
+		if (isalpha(buf)) cout << " " << buf << "\n";
+	}
+	system("pause");
+	return 0;
+}
